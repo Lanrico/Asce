a8f049c2b4fcb957c3edf2848a9a44f1ccf7ead1
weland <weland@bupt.edu.cn>
2022-04-10 19:18:42 +0800

    dev1 init commit

---
+++ plugin/popup.js
@@ -0,0 +1,37 @@
+var btn = document.getElementById("btn");
+btn.onclick = function() {
+  // chrome.tabs.getSelected(null, function(tab) {
+  //   var tid=tab.id;
+  //   alert(tid.url);
+  // });
+  /* 
+     获取当前 tab id url
+  */
+  chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+  }, function(tabs) {
+    var tab = tabs[0];
+    console.log(tab.url);
+
+    /*正则分析该 url*/
+    var parse_url = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
+    var url = tab.url;
+    result = parse_url.exec(url);
+    if(result[3]=="www.bilibili.com"){
+      var parse_res5=/^(?:([a-z]+)\/)([A-Za-z]+)/;
+      alert(result[5]);
+      var res5_url=result[5];
+      res5=parse_res5.exec(res5_url);
+      if(res5[1]=="video"){
+        alert("yes!");
+        alert(result[1]+result[2]+result[3]+result[5]);
+      }else{
+        alert("not a video");
+      }
+    }else{
+      alert("not bilibili");
+    }
+  });
+
+};
