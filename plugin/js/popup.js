95c071ac8cade6fdbe40c346c6dce7a20a9ef559
weland <weland@bupt.edu.cn>
2022-04-10 20:49:44 +0800

    content connect

--- plugin/js/popup.js
+++ plugin/js/popup.js
@@ -18,20 +18,34 @@ btn.onclick = function() {
     var parse_url = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
     var url = tab.url;
     result = parse_url.exec(url);
-    if(result[3]=="www.bilibili.com"){
-      var parse_res5=/^(?:([a-z]+)\/)([A-Za-z]+)/;
+    if (result[3] == "www.bilibili.com") {
+      var parse_res5 = /^(?:([a-z]+)\/)([A-Za-z]+)/;
       alert(result[5]);
-      var res5_url=result[5];
-      res5=parse_res5.exec(res5_url);
-      if(res5[1]=="video"){
+      var res5_url = result[5];
+      res5 = parse_res5.exec(res5_url);
+      if (res5[1] == "video") {
         alert("yes!");
-        alert(result[1]+result[2]+result[3]+result[5]);
-      }else{
+        alert(result[1] + result[2] + result[3] + result[5]);
+
+      } else {
         alert("not a video");
       }
-    }else{
+    } else {
       alert("not bilibili");
     }
   });
 
+};
+var ol = document.getElementById("ol");
+ol.onclick = function() {
+  chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+  }, function(tabs) {
+    var tab = tabs[0];
+    chrome.tabs.sendMessage(tab.id, { greeting: "hello" },
+      function(response) {
+        console.log(response);
+      });
+  });
 };
