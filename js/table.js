4d80977ac0ae5cf6485e4d8164e1252a27ac9ae6
weland <weland@bupt.edu.cn>
2022-05-01 22:18:28 +0800

    split page

--- js/table.js
+++ js/table.js
@@ -1,9 +1,8 @@
 
 //document.getElementsByTagName("BODY")[0].onpageshow = function() { showTable(); };
 $(document).ready(function() {
-  showTable();
-  
-  $(".rating").rating();
+  console.log("split");
+  splitTable();
 
   var delete_EL = document.getElementById("delete");
   delete_EL.onclick = function() {
@@ -21,47 +20,45 @@ $(document).ready(function() {
     });
   };
 });
-function showTable() {
-  chrome.storage.sync.get('table', function(result) {
-    table = result.table;
-    var str = "";
-    for (var i = 0; i <= table.length - 1; i++) {
-      str += '<tr>';
+function showTable(startRow, endRow, table) {
+  var str = "";
+  for (var i = startRow-1; i < endRow; i++) {
+    console.log("llll");
+    str += '<tr>';
 
-      // title
-      str += '<td>';
-      str += table[i]['title'];
-      str += '</td>';
-      str += '<td>';
+    // title
+    str += '<td>';
+    str += table[i]['title'];
+    str += '</td>';
+    str += '<td>';
 
-      // User icon and name
-      var asoul = table[i]['asoul'];
-      str += '<h4 class=\"ui image header\">';
-      str += `<img src="/img/bag/${asoul}.png" class="ui mini rounded image">`;
-      str += '<div class="content">';
-      str += table[i]['user'];
-      str += '</div></h4></td>';
+    // User icon and name
+    var asoul = table[i]['asoul'];
+    str += '<h4 class=\"ui image header\">';
+    str += `<img src="/img/bag/${asoul}.png" class="ui mini rounded image">`;
+    str += '<div class="content">';
+    str += table[i]['user'];
+    str += '</div></h4></td>';
 
-      // link and BV
-      var link = 'https://bilibili.com/video/' + table[i]['link'];
-      str += '<td>';
-      str += `<a href=${link}>`;
-      var BV = table[i]['link'];
-      str += BV;
-      str += '</a>';
-      str += '</td>';
+    // link and BV
+    var link = 'https://bilibili.com/video/' + table[i]['link'];
+    str += '<td>';
+    str += `<a href=${link}>`;
+    var BV = table[i]['link'];
+    str += BV;
+    str += '</a>';
+    str += '</td>';
 
-      // rating
-      str += '<td><div class=\"ui rating\"></div></td>';
+    // rating
+    str += '<td><div class=\"ui rating\"></div></td>';
 
-      // delete
-      str += `<td><button class="ui button" id="delete" bv="${BV}">delete</button></td>`;
+    // delete
+    str += `<td><button class="ui button" id="delete" bv="${BV}">delete</button></td>`;
 
-      str += '</tr>';
-    }
-    contain = document.getElementById("contain");
-    contain.innerHTML = str;
-  });
+    str += '</tr>';
+  }
+  contain = document.getElementById("contain");
+  contain.innerHTML = str;
   $(".rating").rating();
 
 };
@@ -72,3 +69,27 @@ clear.onclick = function() {
 };
 
 
+function splitTable() {
+  chrome.storage.sync.get('table', function(result) {
+    table = result.table;
+    var num = table.length;//表格所有行数(所有记录数)
+    var totalPage = 0;//总页数
+    var pageSize = 4;//每页显示行数
+    //总共分几页
+    if (num / pageSize > parseInt(num / pageSize)) {
+      totalPage = parseInt(num / pageSize) + 1;
+    } else {
+      totalPage = parseInt(num / pageSize);
+    }
+    var currentPage = 1;//当前页数
+    var startRow = (currentPage - 1) * pageSize + 1;//开始显示的行  1
+    var endRow = currentPage * pageSize;//结束显示的行   15
+    endRow = (endRow > num) ? num : endRow;
+    showTable(startRow, endRow, table);
+  });
+}
+
+
+function showFoot(){
+  var footStr="";
+}
