e6ccea9e949bb08b693b0cb02b194b61cd2cd317
Lanrico <228298909@qq.com>
2022-05-02 21:23:08 +0800

    去掉了表单

--- js/table.js
+++ js/table.js
@@ -133,7 +133,7 @@ function showTable(startRow, endRow, table) {
 
     // delete
     // str += `<td><button class="${button_dic[asoul]}" id="delete" bv="${BV}">删除</button></td>`;
-    str += `<td><form><button class="${button_dic[asoul]}" id="delete" bv="${BV}" type = submit>删除</button></form></td>`;
+    str += `<td><button class="${button_dic[asoul]}" id="delete" bv="${BV}">删除</button></td>`;
 
     str += '</tr>';
   }
